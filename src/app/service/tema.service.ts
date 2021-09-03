de3f14d2dcee168cc43b0ce17230595fbec280e5
andrem91 <andrem166@gmail.com>
2021-09-03 09:46:22 -0300

    implementação do put e delete de temas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -16,11 +16,22 @@ export class TemaService {
   }
 
   getAllTema(): Observable<Tema[]> {
-    return this.http.get<Tema[]>('https://bloguniversocervejeiro.herokuapp.com/temas', this.token)
+    return this.http.get<Tema[]>('https://bloguniversocervejeiro.herokuapp.com/temas', this.token);
+  }
+
+  getByIdTema(id: Number): Observable<Tema> {
+    return this.http.get<Tema>(`https://bloguniversocervejeiro.herokuapp.com/temas/${id}`, this.token);
   }
 
   postTema(tema: Tema): Observable<Tema> {
-    return this.http.post<Tema>('https://bloguniversocervejeiro.herokuapp.com/temas', tema, this.token)
+    return this.http.post<Tema>('https://bloguniversocervejeiro.herokuapp.com/temas', tema, this.token);
   }
 
+  putTema(tema: Tema): Observable<Tema> {
+    return this.http.put<Tema>('https://bloguniversocervejeiro.herokuapp.com/temas', tema, this.token);
+  }
+
+  deleteTema(id: Number) {
+    return this.http.delete(`https://bloguniversocervejeiro.herokuapp.com/temas/${id}`, this.token);
+  }
 }
