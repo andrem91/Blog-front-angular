bebc1d1e1d176dd1418c18392b2626253d791fd3
andrem91 <andrem166@gmail.com>
2021-09-06 12:45:23 -0300

    Implementação POST e GET de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -10,9 +10,12 @@ import { UserLogin } from '../model/UserLogin';
 })
 export class AuthService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
 
   entrar(userLogin: UserLogin) : Observable<UserLogin> {
     return this.http.post<UserLogin>('https://bloguniversocervejeiro.herokuapp.com/usuarios/logar', userLogin);
@@ -20,6 +23,10 @@ export class AuthService {
 
   cadastrar(user: User) : Observable<User> {
     return this.http.post<User>('https://bloguniversocervejeiro.herokuapp.com/usuarios/cadastrar', user);
+  }
+
+  getByIdUser(id: number): Observable<User> {
+    return this.http.get<User>(`https://bloguniversocervejeiro.herokuapp.com/usuarios/${id}`, this.token);
   }
 
   logado() {
